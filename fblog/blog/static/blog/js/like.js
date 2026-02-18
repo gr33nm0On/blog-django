e0e407bb0e08724a4b30b2dc6bba6042285fb841
gr33nm0On <d2ae358d42d04672@proton.me>
2026-02-19 01:42:20 +0300

    one more hotfix

--- fblog/blog/static/blog/js/like.js
+++ fblog/blog/static/blog/js/like.js
@@ -1,14 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
     document.querySelectorAll('[id^="btn-like-"]').forEach(btn => {
-        const postId = btn.dataset.postId;
-        const userId = btn.dataset.userId;
-
-        label = document.getElementById('label-post-like-' + postId)
+        const postId = btn.dataset.postId; // data-post-id
+        const userId = btn.dataset.userId; // data-user-id
 
         btn.addEventListener("click", function() {
             fetch(`/like/post/${postId}/${userId}`)
                 .then(response => response.json())
                 .then(data => {
+                    label = document.getElementById('label-post-like-' + postId)
                     if (data.liked) {
                         btn.innerHTML = "unlike";
                         label.innerHTML = parseInt(label.innerHTML, 10) + 1
